d7c67ee7be398d2d701c669ddd1c554a78a275eb
Yuri89 <69095138+Yuri89@users.noreply.github.com>
2023-04-05 16:18:36 -0300

    Add files via upload

---
+++ funcoes.js
@@ -0,0 +1,30 @@
+function soma (a,b){
+    return a + b;
+}
+function sub (a,b){
+    return a - b;
+}
+function div (a,b){
+    return a / b;
+}
+function mult (a,b){
+    return a * b;
+}
+function potencia (a,b){
+    return a ** b;
+}
+function media4 (a,b,c,d){
+    return (a + b + c+ d)/4;
+}
+function maiorOUmenor (a,b){
+    
+    if(a>b){
+        return valorB = a+" maior do que "+b; 
+        }else{
+        return valorB = a+" menor do que "+b;   
+        }
+        
+}
+
+
+console.log(maiorOUmenor(5,6))
